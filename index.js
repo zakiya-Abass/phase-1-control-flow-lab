fdcd9f869e2d00920b5061c45720d996134ce64c
Zakiya-Abass <zakiyaabass@icloud.com>
2023-01-11 18:06:01 +0300

    controlflow

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 400 && distance < 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance > 2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
+  if (tip === "generous") {
+    return "Thank you so much.";
+  } else if (tip === "not as generous") {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
   // Write your code here!
 }
